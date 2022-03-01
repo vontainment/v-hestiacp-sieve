67c8d4f928935c646aa3e36a39a7e10f9e5cf742
Nikolai X. Shadeauxs <services@vontainment.com>
2022-02-28 23:38:18 -0500

    no message

---
+++ README.md
@@ -0,0 +1,5 @@
+# v-hestiacp-sieve
+Autolearn spam ham
+Clone repo to root, Run installspam.sh
+It has files and dirs, the script moves them to thier place and sets premissions.
+Cron runs daily adding to athe ll email moved from spam to anywhere but trash as HAM and anything moved to spam as Spam.
---
+++ installspam.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+cp -r /root/hestia-sieve-spamham/conf.d/20-spamham.conf /etc/dovecot/conf.d/05-spamham.conf
+cp -r /root/hestia-sieve-spamham/mail/. /var/mail/
+cp -r /root/hestia-sieve-spamham/sieve/. /etc/dovecot/sieve/
+
+chown -R mail:mail /var/mail
+chmod -R 0775 /var/mail/imapsieve_copy
+chmod 0755 /etc/dovecot/sieve/*
+chmod 0775 /var/mail/sieve/
+chmod 0644 /var/mail/sieve/*
+
+service dovecot restart
+
+# su -s /bin/bash debian-spamd -c "sa-learn --dump magic"
+
+cat <<EOF | sudo tee /etc/cron.daily/spamham
+#!/usr/bin/env
+bash /etc/dovecot/sieve/scan_reported_mails
+EOF
+
+chmod 755 /etc/cron.daily/spamham && chown root:root /etc/cron.daily/spamham
