4545a00f9f3af83e1557b6117be42c7e8e17cc4b
SPECTRENXS\djav1 <nikolai@vontainment.com>
2022-11-20 13:17:51 -0500

    error fixes

--- installspam.sh
+++ installspam.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-cp -r /root/hestia-sieve-spamham/conf.d/20-spamham.conf /etc/dovecot/conf.d/05-spamham.conf
-cp -r /root/hestia-sieve-spamham/mail/. /var/mail/
-cp -r /root/hestia-sieve-spamham/sieve/. /etc/dovecot/sieve/
+cp -r ./sieve/conf.d/20-spamham.conf /etc/dovecot/conf.d/20-spamham.conf
+cp -r ./mail/. /var/mail/
+cp -r ./sieve/. /etc/dovecot/sieve/
+mkdir -p /var/mail/imapsieve_copy
+mkdir -p /var/mail/imapsieve_copy/spam
+mkdir -p /var/mail/imapsieve_copy/ham
 
 chown -R mail:mail /var/mail
 chmod -R 0775 /var/mail/imapsieve_copy
