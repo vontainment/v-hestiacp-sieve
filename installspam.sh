7cf561f0d5a41de72ad1c184d48332afd614b445
sahsanu <sahsanu@users.noreply.github.com>
2023-07-27 20:42:57 +0200

    Fix spamham cron.daily script

--- installspam.sh
+++ installspam.sh
@@ -18,8 +18,8 @@ service dovecot restart
 # su -s /bin/bash debian-spamd -c "sa-learn --dump magic"
 
 cat <<EOF | sudo tee /etc/cron.daily/spamham
-#!/usr/bin/env
-bash /etc/dovecot/sieve/scan_reported_mails
+#!/usr/bin/env bash
+/etc/dovecot/sieve/scan_reported_mails
 EOF
 
 chmod 755 /etc/cron.daily/spamham && chown root:root /etc/cron.daily/spamham
